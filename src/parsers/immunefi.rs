0383934ce246b743dfd6ea7c2a8c56103b5bd7cd
Audit Your Contracts <audityourcontracts@gmail.com>
2023-06-30 20:49:58 +1000

    Semaphore FTW!

--- src/parsers/immunefi.rs
+++ src/parsers/immunefi.rs
@@ -7,6 +7,9 @@ use crate::github_api;
 use crate::parsers::parse::Repo;
 use tokio::task::{spawn_blocking, spawn};
 use tokio::time::Duration;
+use tokio::time::Instant;
+use std::sync::Arc;
+use tokio::sync::Semaphore;
 
 pub struct ImmunefiParser {
     pub url: String,
@@ -67,35 +70,58 @@ impl ImmunefiParser {
 
         // For each bounty URL navigate to it and see if there's anyt github links in there.
         let base_url = "https://immunefi.com";
-        let selector = Selector::parse("a").unwrap();
+        let mut tasks = Vec::new();
+
+        // Rate-limiting variables
+        let max_concurrent_requests = 30; // Set the maximum number of concurrent requests
+        let semaphore = Arc::new(Semaphore::new(max_concurrent_requests));
 
         for bounty_url in unique_links.into_iter().collect::<Vec<String>>() {
-            let full_url = format!("{}{}", base_url, bounty_url);
-            log::info!("Parsing url {}", full_url);
+            let semaphore = Arc::clone(&semaphore); 
 
-            let response = reqwest::get(&full_url).await?;
-            let body = response.text().await?;
-            let document = Html::parse_document(&body);
+            // Spawn a task for each bounty URL
+            let task = tokio::spawn(async move {
+                let permit = semaphore.acquire().await.expect("Failed to acquire semaphore permit"); 
+                let selector = Selector::parse("a").unwrap();
+                let full_url = format!("{}{}", base_url, bounty_url);
+                log::info!("Parsing url {}", full_url);
 
-            for element in document.select(&selector) {
-                if let Some(link) = element.value().attr("href") {
-                    if link.contains("github.com") && !link.contains("immunefi-team") {
-                        // Parse for the github repo format
-                        let parsed_url = Url::parse(&link);
-                        if let Ok(url) = parsed_url {
-                            let path_segments = url.path_segments().unwrap();
-                            let formatted_path = path_segments.take(2).collect::<Vec<_>>().join("/");
-                            let formatted_url = format!("{}://{}/{}", url.scheme(), url.host_str().unwrap(), formatted_path);
-                            if unique_github_links.insert(formatted_url.to_owned()) {
-                                // Only logging on new github urls
-                                log::info!("Found github link: {}", formatted_url);
+                let response = reqwest::get(&full_url).await?;
+                let body = response.text().await?;
+                let document = Html::parse_document(&body);
+
+                let mut github_links = HashSet::new();
+
+                for element in document.select(&selector) {
+                    if let Some(link) = element.value().attr("href") {
+                        if link.contains("github.com") && !link.contains("immunefi-team") {
+                            log::debug!("Found github url {}", link);
+                            // Parse for the github repo format
+                            let parsed_url = Url::parse(&link);
+                            if let Ok(url) = parsed_url {
+                                let path_segments = url.path_segments().unwrap();
+                                let formatted_path = path_segments.take(2).collect::<Vec<_>>().join("/");
+                                let formatted_url = format!("{}://{}/{}", url.scheme(), url.host_str().unwrap(), formatted_path);
+                                github_links.insert(formatted_url.to_owned());
+                            } else {
+                                log::error!("Couldn't parse the url {}", link)
                             }
-                        } else {
-                            log::error!("Couldn't parse the url {}", link)
                         }
                     }
                 }
-            }
+                drop(permit);
+                Ok::<HashSet<String>, Box<dyn std::error::Error + Send + Sync>>(github_links)
+            });
+            tasks.push(task);
+        }
+
+        // Wait for all tasks to complete concurrently
+        let results = futures::future::try_join_all(tasks).await?;
+        
+        // Iterate over the results and collect the unique GitHub links
+        for result in results {
+            let github_links = result?;
+            unique_github_links.extend(github_links);
         }
 
         for github_link in unique_github_links {
